c135db3f8c996b2ed938f8fa3d33a3ab515bf907
matthew <matthewyt765@gmail.com>
2025-04-07 23:42:00 +0800

    add require

--- sections/sign-up.php
+++ sections/sign-up.php
@@ -23,8 +23,8 @@
     <form id="signupForm" class="form-control-sm" method="post" action="register.php">
       <label for="full_name" class="form-label">First and last name</label>
       <div class="input-group">
-        <input type="text" id="full_name" name="first_name" aria-label="First name" class="form-control">
-        <input type="text" id="full_name" name="last_name" aria-label="Last name" class="form-control">
+        <input type="text" id="full_name" name="first_name" aria-label="First name" class="form-control"require>
+        <input type="text" id="full_name" name="last_name" aria-label="Last name" class="form-control"require>
       </div>
       <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address (Institutional Email)</label>
@@ -34,25 +34,19 @@
       class="form-control"
       id="exampleInputEmail1"
       placeholder="Enter your IE"
-      
+      require
       >
       <small id="email-msg" style="color: red;"></small>
     </div>
-      <!-- <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address (Institutional Email)</label>
-        <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your IE"  pattern="[a-zA-Z0-9._%+-]+@plmun\.edu\.ph$" 
-        title="Only @plmun.edu.ph emails are allowed" required>
-        <small id="email-msg" style="color: red;"></small>
-      </div> -->
 
       <div class="input mb-3">
         <label for="student_num" class="form-label">Student number</label>
-        <input type="number" name="student_num" min="0" max="99999999" oninput="if(this.value.length > 8) this.value = this.value.slice(0, 8)" class="form-control" id="student_num" aria-describedby="emailHelp" placeholder="Enter your Student number" >
+        <input type="number" name="student_num" min="0" max="99999999" oninput="if(this.value.length > 8) this.value = this.value.slice(0, 8)" class="form-control" id="student_num" aria-describedby="emailHelp" placeholder="Enter your Student number" require>
       </div>
   
       <div class="mb-3">
         <p>Select your Program</p>
-        <select class="form-select" name="program" aria-label="Default select example" aria-placeholder="Select your Program--" >
+        <select class="form-select" require name="program" aria-label="Default select example" aria-placeholder="Select your Program--" >
           <option value="notProgram" selected>Lists of Programs</option>
           <option value="MBA">Master in Business Administration</option>
           <option value="MAE-EM">Master of Arts in Education, major in Educational Management</option>
@@ -83,12 +77,12 @@
 
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" name="password" class="form-control" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Enter your desired password" >
+        <input type="password" name="password" class="form-control" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Enter your desired password" require>
       </div>
 
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-        <input type="password" name="confirm_password"  class="form-control" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Re-enter your password" >
+        <input type="password" name="confirm_password" require class="form-control" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Re-enter your password" >
       </div>
 
       <div style="text-align: center;">

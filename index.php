55008dda08f4973420114fbcc1c29626c8e0a25a
matthew <matthewyt765@gmail.com>
2025-04-07 23:25:00 +0800

    add header footer

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <?php 
-  include("db_connection.php");
-  
-
-  try {
-    $connect = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $port);
-  } catch(mysqli_sql_exception) {
-    echo "You're not connected";
-  }   
-  if($connect) {
-    echo "You're connected!";
-  }
+include('components/header.php');
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>PLMun Connect</title>
-  <link rel="icon" type="image/png" href="./assets/images/Logo.png">
-  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="custom.css">
-  <script src="assets/js/bootstrap.bundle.min.js" defer></script>
-</head>
 
 <body>
   <nav class="navbar navbar-expand-lg bg-body-primary">

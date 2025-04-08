879add8994615cb3432437eeb82c826d0ec15ac4
matthew <matthewyt765@gmail.com>
2025-04-08 12:54:57 +0800

    many changes cant track error HAHA

---
+++ db_func.php
@@ -0,0 +1,17 @@
+<?php 
+  function getDatabaseConnection() {
+    $db_server = 'localhost';
+    $db_user = 'root';
+    $db_pass = "";
+    $db_name = "plmun_connect";
+    $connect = "";
+    $port = "3307";
+
+    $connect = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $port);
+    if($connect->connect_error) {
+      die("error failed" . $connect -> connect_error);
+    }
+
+    return $connect;
+  }
+?>
--- sections/sign-up.php
+++ sections/sign-up.php
@@ -1,3 +1,78 @@
+<?php
+  include '../db_func.php';
+  $dbConnection = getDatabaseConnection();
+
+
+  $firstName = "";
+  $lastName = "";
+  $studentNum = "";
+  $emailIE = "";
+  $program = "";
+  $password = "";
+  $confirmPass = "";
+
+  $firstName_error = "";
+  $lastName_error = "";
+  $studentNum_error = "";
+  $emailIE_error = "";
+  $program_error = "";
+  $password_error = "";
+  $confirmPass_error = "";
+
+  $error = false;
+
+  if($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $firstName = $_POST["first_name"];
+    $lastName = $_POST["last_name"];
+    $studentNum = $_POST["student_num"];
+    $emailIE = $_POST["email"];
+    $program = $_POST["program"];
+    $password = md5($_POST["password"]);
+    $confirmPass = md5($_POST["confirm_password"]);
+
+    if(empty($firstName)) {
+      $firstName_error = "First name is required";
+      $error = true;
+    }
+  
+    if(empty($lastName)) {
+      $firstName_error = "Last name is required";
+      $error = true;
+    }
+  
+    if($studentNum < 7 && $studentNum >8) {
+      $studentNum_error = "8 is the number of student number";
+      $error = true;
+    }
+  
+    if(!preg_match("/^[a-zA-Z0-9._%+-]+@plmun\.edu\.ph$/", $emailIE)){
+      $emailIE_error = "Email format incorrect not ie";
+      $error = true;
+    }
+
+    // verify if the emeail is exist
+  
+    if($program == "" || $program == "notProgram") {
+      $program_error = "Not a valid program";
+      $error = true;
+    }
+  
+    if(strlen($password) < 8) {
+      $password_error = "Password must have atleast 8 characters";
+      $error = true;
+    }
+  
+    if($confirmPass != $password) {
+      $confirmPass_error = "password and confirm password are not the same";
+      $error = true;
+    }
+  }
+
+  
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,33 +95,29 @@
   <div class="container d-flex align-items-center justify-content-center" id="sign-up" style='min-height: 100vh;'>
   <div class="card p-2" style="width: 50%;">
     <h2 class="text-center">Sign up</h2>
-    <form id="signupForm" class="form-control-sm" method="post" action="register.php">
+    <form class="form-control-sm" method="post" >
       <label for="full_name" class="form-label">First and last name</label>
       <div class="input-group">
-        <input type="text" id="full_name" name="first_name" aria-label="First name" class="form-control"require>
-        <input type="text" id="full_name" name="last_name" aria-label="Last name" class="form-control"require>
+        <input type="text" id="full_name" name="first_name" aria-label="First name" class="form-control" value="<?= $firstName ?>" require>
+        <small class="text-danger"><?= $firstName_error ?></small>
+        <input type="text" id="full_name" name="last_name" aria-label="Last name" class="form-control" value="<?= $lastName ?>" require>
+        <small class="text-danger"><?= $lastName_error ?></small>
       </div>
       <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address (Institutional Email)</label>
-    <input
-      type="email"
-      name="email"
-      class="form-control"
-      id="exampleInputEmail1"
-      placeholder="Enter your IE"
-      require
-      >
-      <small id="email-msg" style="color: red;"></small>
-    </div>
+        <label for="email" class="form-label">Email address (Institutional Email)</label>
+        <input type="email" name="email" value="<?= $emailIE ?>" class="form-control" id="email" placeholder="Enter your IE" require>
+        <small class="text-danger"><?= $emailIE_error ?></small>
+      </div>
 
       <div class="input mb-3">
         <label for="student_num" class="form-label">Student number</label>
-        <input type="number" name="student_num" min="0" max="99999999" oninput="if(this.value.length > 8) this.value = this.value.slice(0, 8)" class="form-control" id="student_num" aria-describedby="emailHelp" placeholder="Enter your Student number" require>
+        <input type="number" value="<?= $studentNum ?>" name="student_num" min="0" max="99999999" oninput="if(this.value.length > 8) this.value = this.value.slice(0, 8)" class="form-control" id="student_num" aria-describedby="emailHelp" placeholder="Enter your Student number" require>
+        <small class="text-danger"><?= $studentNum_error ?></small>
       </div>
   
       <div class="mb-3">
         <p>Select your Program</p>
-        <select class="form-select" require name="program" aria-label="Default select example" aria-placeholder="Select your Program--" >
+        <select class="form-select" value="<?= $program?>" name="program" aria-label="Default select example" aria-placeholder="Select your Program--" require>
           <option value="notProgram" selected>Lists of Programs</option>
           <option value="MBA">Master in Business Administration</option>
           <option value="MAE-EM">Master of Arts in Education, major in Educational Management</option>
@@ -73,20 +144,23 @@
           <option value="BAPS">Bachelor of Arts in Political Science</option>
           <option value="BSSW">Bachelor of Science in Social Work</option>
         </select>
+        <small class="text-danger"><?= $emailIE_error ?></small>
       </div>
 
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" name="password" class="form-control" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Enter your desired password" require>
+        <input type="password" name="password"  class="form-control" value="<?= $password ?>" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Enter your desired password" require>
+        <small class="text-danger"><?= $password_error ?></small>
       </div>
 
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-        <input type="password" name="confirm_password" require class="form-control" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Re-enter your password" >
+        <input type="password" name="confirm_password" class="form-control" value="<?= $confirmPass ?>" maxlength="50" minlength="8" id="exampleInputPassword1" placeholder="Re-enter your password" require>
+        <small class="text-danger"><?= $confirmPass_error ?></small>
       </div>
 
       <div style="text-align: center;">
-      <button type="submit" class="btn btn-primary" name="signUp">Sign Up</button>
+        <button type="submit" class="btn btn-primary" name="signUp">Sign Up</button>
       </div>
 
     </form>

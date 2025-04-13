0c5cdf649fe487bcd411b5cb8ee21947ac9854e6
matthew <matthewyt765@gmail.com>
2025-04-13 23:54:44 +0800

    add modal, customize event view more and add create the event

--- createEvent.php
+++ createEvent.php
@@ -13,11 +13,63 @@
         include './section_components/header_includes/nav.php';
     ?>
 
-<div class="container mt-3" style="min-height: 100vh;">
-    <div class="d-flex justify-content-between align-items-center">
-        <h1 class="m-0">Create a Event!</h1>
-        <div id="datetimes" class="text-end"></div>
+<div class="container mt-3">
+<div class="container d-flex justify-content-center align-items-center mt-5 min-vh-50">
+    <div class="w-50 shadow p-4 rounded bg-light">
+
+    <h3 class="text-center mb-4">Organization Form</h3>
+
+    <form>
+        <!-- Organization Name -->
+        <div class="mb-3">
+            <label for="orgName" class="form-label">Organization Name</label>
+            <select class="form-select" id="organization" name="organization" required>
+                <option value="" disabled selected>Select an organization</option>
+                <option value="CAS">CAS</option>
+                <option value="CBA">CBA</option>
+                <option value="CCJ">CCJ</option>
+                <option value="CITCS">CITCS</option>
+                <option value="COA">COA</option>
+                <option value="CTE">CTE</option>
+                <option value="IPPG">IPPG</option>
+                <option value="CS Society">CS Society</option>
+                <option value="IT Society">IT Society</option>
+                <option value="ACT Society">ACT Society</option>
+            </select>
+        </div>
+
+        <!-- Position -->
+        <div class="mb-3">
+            <label for="position" class="form-label">Position</label>
+            <select class="form-select" id="position" name="position" required>
+                <option value="" disabled selected>Select a position</option>
+                <option value="President">President</option>
+                <option value="Vice President">Vice President</option>
+                <option value="Secretary">Secretary</option>
+                <option value="Treasurer">Treasurer</option>
+                <option value="Auditor">Auditor</option>
+                <option value="PRO 1">Public Relations Officer</option>
+                <option value="PRO 2">Public Relations Officer</option> 
+                <option value="Business Manager 1">Business Manager 1</option>
+                <option value="Business Manager 2">Business Manager 2</option>
+                <option value="Sergeant at Arms 1">Sergeant at Arms 1</option>
+                <option value="Sergeant at Arms 2">Sergeant at Arms 2</option>
+                <option value="1st Representative">1st Representative</option>
+                <option value="2nd Representative">2nd Representative</option>
+                <option value="3rd Representative">3rd Representative</option>
+                <option value="4th Representative">4th Representative</option>
+                <option value="4th Representative">Volunteer/Junior Officer</option>
+            </select>
+        </div>
+
+        <!-- Submit Button -->
+        <div class="d-grid">
+            <button type="submit" class="btn btn-success">Request</button>
+        </div>
+      </form>
+
     </div>
+  </div>
 </div>
     <script>
         function updateDateTime() {
--- section_components/body_includes/body_authenticated.php
+++ section_components/body_includes/body_authenticated.php
@@ -1,20 +1,72 @@
-
 <div class="container mt-3" style="min-height: 100vh;">
-<div class="d-flex justify-content-between align-items-center">
-    <h1 class="m-0">Welcome <?= $_SESSION['first_name'] ?></h1>
-    <div id="datetimes" class="text-end"></div>
-</div>
+  <div class="d-flex justify-content-between align-items-center">
+      <h1 class="m-0">Welcome <?= $_SESSION['first_name'] ?></h1>
+      <div id="datetimes" class="text-end"></div>
+  </div>
 
-<p>These are the Incoming Events at PLMun!</p>
-    <iframe src="incomingEvents.php" 
-        width="100%" 
-        height="400" 
-        style="border: 1px solid #ccc;"
-        iframeborder="0">
-        sd
-    </iframe>
-</div>
+  <p>These are the Incoming Events at PLMun!</p>
 
+  <div class="card m-3 border border-success">
+    <div class="card-header ">
+      <b>CITCS</b>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">Monthly Kamustahan</h5>
+      <p class="card-text">
+        1:00PM - 5:00PM <br>
+        Computer Science Society <br>
+        CL1
+
+      </p>
+      <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        View more 
+      </button>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content">
+
+            <div class="modal-body">
+              <div class="d-flex justify-content-end mb-2">
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="container">
+              
+              <div class="row">
+                <div class="col-7">
+                <h1> Event title</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis adipisci distinctio pariatur libero commodi ea 
+                    vel minus fugiat asperiores, excepturi, officia unde voluptatum reiciendis eligendi ex, quaerat atque. Maiores
+                    officia nesciunt sed architecto ex hic, itaque ab, ea, voluptas reprehenderit aperiam quidem! Itaque illo, 
+                    voluptates veritatis nisi blanditiis consectetur voluptatum voluptate quidem vitae iusto corporis consequuntur,
+                    facilis totam. Dolores quasi quod soluta ipsum! Sint tenetur nobis minus numquam placeat enim!
+                  </p>
+                  <p style="font-weight: bolder;">
+                    Tuesday, April 1 <br>
+                    8:00PM - 10:00PM
+                  </p>
+                  <h3 style="font-weight: 650;">Host</h3>
+                  <p>Matthew David C. Fernandez</p>
+
+                  <button class="btn btn-success mb-3">Count me in!</button>
+                </div>
+                <div class="col-5">
+                  <div class="border border-success mt-2" style="width: 98%; height: 80%;">
+
+                  </div>
+
+                </div>
+              </div>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <script>
     function updateDateTime() {
         var now = new Date();

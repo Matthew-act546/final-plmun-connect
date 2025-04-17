aea17b587c8219bfded5bdee5185b16fea7e9afd
matthew <matthewyt765@gmail.com>
2025-04-17 11:34:49 +0800

    add feaature: if the event is passed then it will not output in theindex .pho

--- section_components/body_includes/body_authenticated.php
+++ section_components/body_includes/body_authenticated.php
@@ -31,53 +31,56 @@
   </div>
   ';
   
-  while ($row = $result->fetch_assoc()) {
-    echo '
-    <div class="container mt-3">
-    
-      <div class="card m-3 border border-success">
-        <div class="card-header">
-          <b>' . htmlspecialchars($row['Host']) . '</b>
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">' . htmlspecialchars($row['Title']) . '</h5>
-          <p class="card-text">
-            ' . htmlspecialchars($row['timeStart']) . ' - ' . htmlspecialchars($row['timeEnd']) . '<br>
-            ' . htmlspecialchars($row['Host']) . '<br>
-            ' . htmlspecialchars($row['Venue']) . '
-          </p>
-          <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modal' . $row['event_id'] . '">
-            View more 
-          </button>
   
-          <!-- Modal -->
-          <div class="modal fade" id="modal' . $row['event_id'] . '" tabindex="-1" aria-labelledby="modalLabel' . $row['event_id'] . '" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-              <div class="modal-content">
-                <div class="modal-body">
-                  <div class="d-flex justify-content-end mb-2">
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                  <div class="container">
-                    <h1>' . htmlspecialchars($row['Title']) . '</h1>
-                    <p>' . nl2br(htmlspecialchars($row['Description'])) . '</p>
-                    <p style="font-weight: bolder;">
-                      ' . htmlspecialchars($row['EventDate']) . '<br>
-                      ' . htmlspecialchars($row['timeStart']) . ' - ' . htmlspecialchars($row['timeEnd']) . '<br>
-                      Venue: ' . htmlspecialchars($row['Venue']) . '
-                    </p>
-                    <h3 style="font-weight: 650;">Host</h3>
-                    <p>' . htmlspecialchars($row['Host']) . '</p>
-                    <button class="btn btn-success mb-3">Count me in!</button>
+  while ($row = $result->fetch_assoc()) {
+    if(date('Y-m-d') < $row['EventDate']) {
+      echo '
+      <div class="container mt-3">
+      
+        <div class="card m-3 border border-success">
+          <div class="card-header">
+            <b>' . htmlspecialchars($row['Host']) . '</b>
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">' . htmlspecialchars($row['Title']) . '</h5>
+            <p class="card-text">
+              ' . htmlspecialchars($row['timeStart']) . ' - ' . htmlspecialchars($row['timeEnd']) . '<br>
+              ' . htmlspecialchars($row['Host']) . '<br>
+              ' . htmlspecialchars($row['Venue']) . '
+            </p>
+            <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modal' . $row['event_id'] . '">
+              View more 
+            </button>
+    
+            <!-- Modal -->
+            <div class="modal fade" id="modal' . $row['event_id'] . '" tabindex="-1" aria-labelledby="modalLabel' . $row['event_id'] . '" aria-hidden="true">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-body">
+                    <div class="d-flex justify-content-end mb-2">
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="container">
+                      <h1>' . htmlspecialchars($row['Title']) . '</h1>
+                      <p>' . nl2br(htmlspecialchars($row['Description'])) . '</p>
+                      <p style="font-weight: bolder;">
+                        ' . htmlspecialchars($row['EventDate']) . '<br>
+                        ' . htmlspecialchars($row['timeStart']) . ' - ' . htmlspecialchars($row['timeEnd']) . '<br>
+                        Venue: ' . htmlspecialchars($row['Venue']) . '
+                      </p>
+                      <h3 style="font-weight: 650;">Host</h3>
+                      <p>' . htmlspecialchars($row['Host']) . '</p>
+                      <button class="btn btn-success mb-3">Count me in!</button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div> <!-- end modal -->
+            </div> <!-- end modal -->
+          </div>
         </div>
-      </div>
-      </div>
-    ';
+        </div>
+      ';
+    }
   }
   
 
